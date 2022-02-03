c630fd6ee675662f346c7c3bea922bc9f2e61291
Lacerda53 <guilhermeandrade2013@gmail.com>
2022-02-03 17:24:40 -0300

    adding docker compose

--- server/src/controllers/generateApkController.ts
+++ server/src/controllers/generateApkController.ts
@@ -4,46 +4,45 @@ import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import { writeServiceContainer } from '../helpers/fileServiceHelp';
 
+const fsPromisse = fs.promises;
+
 class GenerateApkController {
     async index(req: Request, res: Response) {
-        try {
 
-            const {
-                repository = `https://${process.env.GITHUB_TOKEN}@github.com/SofitSoftware/sofit-view-mobile.git`,
-                branch,
-                container
-            } = req.body;
-            const id = uuidv4();
+        const {
+            repository = `https://${process.env.GITHUB_TOKEN}@github.com/SofitSoftware/sofit-view-mobile.git`,
+            branch,
+            container
+        } = req.body;
+        const id = uuidv4();
 
-            cd('public')
-            console.log('Clonando Projeto');
-            await asyncExec(`git clone ${repository} ${id} -b ${branch || 'master'}`);
-            cd(id);
+        cd('public')
+        console.log('Clonando Projeto');
+        await asyncExec(`git clone ${repository} ${id} -b ${branch || 'master'}`);
+        cd(id);
 
-            if (container) {
+        if (container) {
+
+            try {
 
                 cd('src/services');
                 await asyncExec('rm -rf api.ts');
-                fs.writeFile('api.ts', writeServiceContainer(container), (err) => {
+                await fsPromisse.writeFile('api.ts', writeServiceContainer(container))
+                console.log(`Apk apontando para container ${container}`);
+            } catch (err) {
 
-                    if (err) throw err;
-
-                    console.log(`Apk apontando para container ${container}`);
-                })
+                return console.log(err);
             }
-            console.log('Instalando Pacotes');
-            await asyncExec('yarn');
-            console.log('Pacotes instalados');
-            cd('android');
-            // console.log('Limpando build');
-            // await asyncExec('./gradlew clean');
-            console.log('Gerando APK');
-            await asyncExec('./gradlew assembleRelease');
-            res.download(`${__dirname}/../../public/${id}/android/app/build/outputs/apk/release/app-release.apk`);
-        } catch {
-
-            res.sendStatus(500);
         }
+        console.log('Instalando Pacotes');
+        await asyncExec('yarn')
+        console.log('Pacotes instalados');
+        cd('android');
+        // console.log('Limpando build');
+        // await asyncExec('./gradlew clean');
+        console.log('Gerando APK');
+        await asyncExec('./gradlew assembleRelease');
+        res.download(`${__dirname}/../../public/${id}/android/app/build/outputs/apk/release/app-release.apk`);
     }
 }
 
